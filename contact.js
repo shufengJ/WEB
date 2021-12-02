6996eb77c1e53d3696b7abab8d51c23eea337e77
shufeng <1225948033@qq.com>
2021-12-02 11:11:10 +0100

    Tp3.1

--- contact.js
+++ contact.js
@@ -34,12 +34,38 @@ document.querySelector('#contact form button')
             hasError = true
         }
 
+
+        if (!hasError) {
+            var tb = document.getElementById('tb')
+            var newtr = document.createElement('tr')
+            tb.appendChild(newtr)
+            var td1 = document.createElement('td')
+            var td2 = document.createElement('td')
+            var td3 = document.createElement('td')
+            var td4 = document.createElement('td')
+            td1.innerHTML = firstnameInput.value;
+            td2.innerHTML = phoneInput.value;
+            td3.innerHTML = mailInput.value;
+            td4.innerHTML = messageInput.value + "<a href='javascript:;'>Supprimer</a>";
+            newtr.appendChild(td1)
+            newtr.appendChild(td2)
+            newtr.appendChild(td3)
+            newtr.appendChild(td4)
+            document.getElementById("form").reset();
+        }
+        var as = document.querySelectorAll('a')
+        for(var i = 0; i<as.length; i++) {
+            as[i].onclick = function () {
+                tb.removeChild(this.parentNode.parentNode)
+            }
+        }
+
         if (hasError) {
             window.alert("erreur_de_formule")
         }
-        if (!hasError) {
-            window.alert(firstnameInput.value + phoneInput.value + mailInput.value + messageInput.value)
-            document.querySelector('#contact form')
-                .submit()
-        }
+
+        //if (!hasError) {
+        //    document.querySelector('#contact form')
+        //        .submit()
+        //}
     })
