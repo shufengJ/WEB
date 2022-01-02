361bfc758c7e8b358da6ccc32503854ac768d041
Kev744 <94228138+Kev744@users.noreply.github.com>
2022-01-02 14:25:34 +0100

    Update Date.js

--- Date.js
+++ Date.js
@@ -1,1 +1,6 @@
+  function date() {const date = new Date();
+    return document.getElementById("date").innerHTML = date.getUTCDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear() + "<br>" + date.getHours() + ":" + date.getMinutes()
+}
+
+console.log(date())
 
